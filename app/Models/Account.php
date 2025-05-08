b4463ecb92c14faa3e9eef04c57b96655ed0d10b
csvitor-dev <csvitor.dev@gmail.com>
2025-05-08 19:54:13 -0300

    feat(models): create factory to mock account

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class Account extends Model
 {
+    use HasFactory;
+
     protected $table = 'account';
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'balance',
+    ];
 }
---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string,mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => fake()->uuid(),
+            'name' => fake()->name(),
+            'balance' => fake()->numberBetween(int2: 9999.99),
+        ];
+    }
+}
